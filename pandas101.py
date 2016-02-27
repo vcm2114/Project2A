7f12c08c33ccc7e1f8e9a050c9daa99d2e7d1c08
ClementGiron <gironclement@orange.fr>
2016-02-27 13:43:27 +0100

    Création d'une fonction donnant tous les concepts formel et fonctions combinatoires

--- pandas101.py
+++ pandas101.py
@@ -86,7 +86,7 @@ print(df)
 
 # Trouver les concepts formels
 
-# Calcule le concept formel d'un ensemble d'attributs
+# Calcule les objets communs d'un ensemble d'attributs
 # i.e. l'ensemble des individus qui ont tous vu tous les films passés en argument
 
 def common_entities(dataframe, attributs):
@@ -107,7 +107,7 @@ print('\n')
 res=common_entities(df,["Jaws","Kill Bill","Rambo"])
 print(res)
 
-# Calcule le concept formel d'un ensemble d'objets
+# Calcule les attributs communs d'un ensemble d'objets
 # i.e. l'ensemble des films qui ont tous été vus par les individus passés en argument
 
 def common_features(dataframe, objets):
@@ -144,3 +144,43 @@ def subtable(t):
     return [t[0:i]+t[i+1:] for i in range(n-1)]+[t[0:n-1]]
 
 print(subtable([1,2,3,4,5,6]))
+
+# traduit un entier en tableau binaire
+
+def int_to_binary(n):
+    if n==0:
+        return [0]
+    elif n==1:
+        return [1]
+    else:
+        res = int_to_binary(n//2)
+        res.insert(0,n%2)
+        return res
+
+# donne toutes les combinaisons possibles de sous-tableau d'un tableau donné
+
+def combinaisons(t):
+    res=[]
+    n=2**len(t)
+    for i in range(n):
+        combi=int_to_binary(i)
+        res.append([t[i] for i in range(len(combi)) if combi[i]==1])
+    return res
+
+print(combinaisons([0,1,2,3,4,5,6]))
+
+# caculer l'ensemble des concepts formels
+
+def formal_concepts(dataframe):
+    res=[]
+    totalObjets=list(dataframe.index.values)
+    aTraiter=combinaisons(totalObjets)
+    while aTraiter != [] :
+        obj=aTraiter[0]
+        att=common_features(dataframe,obj)
+        if is_formal_concept(dataframe,obj,att):
+            res.append((obj,att)) # comment enrgistrer l'ancetre ?
+        aTraiter.remove(obj)
+    return res
+
+print(formal_concepts(df))

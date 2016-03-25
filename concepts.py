e5b4b1375f4bffb1c472386d06ebe25d47814c59
ClementGiron <gironclement@orange.fr>
2016-03-25 11:54:06 +0100

    Inclose, début BFS

--- concepts.py
+++ concepts.py
@@ -9,26 +9,29 @@ class FormalConcept:
     - un ensemble d'objets
     - un ensemble d'attributs"""
     
+    """méthode d'initialisation"""
     def __init__(self,obj,att):
-        self.entities=obj
-        self.features=att
-
-# Définition d'une classe représentant le treilli de concepts
-
-class ConceptLattice:
-    """un treilli de concepts est défini de façon récursive par
-    - un noeud-concept
-    - un ensemble de treillis descendants"""
+        self.objects=obj
+        self.attributes=att
     
-    def __init__(self,concept):
-        self.node=concept
-        self.children=[]
+    """méthode permettant d'ajouter un objet à un concept"""
+    def add_entity(self,entity):
+        self.objects.append(entity)
     
-    def add_child(self,child):
-        self.children.append(child)
+    """méthode permettant d'ajouter un attribut à un concept"""
+    def add_feature(self,feature):
+        self.attributes.append(feature)
+    
+    """méthode facilitant l'affichage console d'un concept"""
+    def __str__(self):
+        return "Objets : {} | Attributs : {}".format(self.objects, self.attributes)
 
 
-# Génération aléatoire d'un tableau de taille m x n avec names et movies prédéfinis #
+
+#------------------------------------#
+# Simulation d'un contexte aléatoire #
+#------------------------------------#
+
 
 def rand2(n,m):
     return np.random.randint(2, size=(n,m))
@@ -36,205 +39,191 @@ def rand2(n,m):
 def create_tab(n,m):
     [names,movies] = import_xls('data.xls',n,m)
     return pd.DataFrame(rand2(n,m),index=names,columns=movies)
+    
 
-# test
-df=create_tab(3,5)
+
+# variables globales
+df = create_tab(5,5)
 print(df)
-
-# Trouver les concepts formels
-
-# Calcule les objets communs d'un ensemble d'attributs
-# i.e. l'ensemble des individus qui ont tous vu tous les films passés en argument
-
-def common_entities(dataframe, attributs):
-
-    ind=list(dataframe.index.values)
-    n=len(ind)
-    obj=[1 for e in range(n)]
-
-    i=0;
-    for e in ind:
-        for f in attributs:
-            obj[i]=obj[i]*dataframe.loc[e,f]
-        i=i+1
-
-    return [ind[i] for i in range(n) if obj[i]==1]
-
-# test
-# print('\n')
-# res=common_entities(df,["Jaws","Kill Bill","Rambo"])
-# print(res)
-
-# Calcule les attributs communs d'un ensemble d'objets
-# i.e. l'ensemble des films qui ont tous été vus par les individus passés en argument
-
-def common_features(dataframe, objets):
-
-    col=list(dataframe.columns.values)
-    n=len(col)
-    att=[1 for e in range(n)]
-
-    i=0;
-    for f in col:
-        for e in objets:
-            att[i]=att[i]*dataframe.loc[e,f]
-        i=i+1
-
-    return [col[i] for i in range(n) if att[i]==1]
-
-# test
-# print('\n')
-# res2=common_features(df,["Bob","Ashley","Jordan"])
-# print(res2)
+A = df.as_matrix()
 
 
-# indique si le couple (objets,attributs) forme un concept formel (résultat booléen)
 
-def is_formal_concept(dataframe,objets,attributs):
-    return (attributs==common_features(dataframe,objets))and(objets==common_entities(dataframe,attributs))
-
-# test
-# print(is_formal_concept(df,["Bob","Ashley"],(common_features(df,["Bob","Ashley"]))))
+#---------------------------------------------------#
+# Trouver les concepts formels - Algorithme InClose #
+#---------------------------------------------------#
 
 
-# traduit un entier en tableau binaire
 
-def int_to_binary(n):
-    if n==0:
-        return [[0],0]
-    elif n==1:
-        return [[1],1]
-    else:
-        res = int_to_binary(n//2)
-        b=n%2
-        res[0].insert(0,b)
-        res[1]+=b
-        return res
+# variables globale
 
-# donne toutes les combinaisons possibles de sous-tableau d'un tableau donné
-
-def combinaisons(t):
-    t_combi=[]
-    n=2**len(t)
-    for i in range(n):
-        combi=int_to_binary(i)[0]
-        t_combi.append([t[i] for i in range(len(combi)) if combi[i]==1])
-    return t_combi
-
-# test
-# print(combinaisons([0,1,2,3,4,5,6]))
-
-# donne toutes les combinaisons possibles de sous-tableau d'un tableau donné,
-# ordonnées par ordre décroissant du nombre d'éléments
-
-def sorted_combinaisons(t):
-    
-    res=[]
-    combi=[]
-    n=len(t)
-    dic_combi=[[] for i in range(n+1)]
-    ncombi=2**n
-    
-    for i in range(ncombi):
-        binary=int_to_binary(i)
-        dic_combi[n-binary[1]].append(binary[0])
-    
-    for e in dic_combi:
-        combi=combi+e
-        
-    for c in combi:
-        res.append([t[i] for i in range(len(c)) if c[i]==1])    
-    
-    return res
-
-#test
-# print(sorted_combinaisons([0,1,2,3,4]))
-
-# caculer l'ensemble des concepts formels
-
-def formal_concepts(dataframe):
-    
-    res=[]
-    totalObjets=list(dataframe.index.values)
-    aTraiter=sorted_combinaisons(totalObjets)
-    
-    for e in aTraiter:
-        att=common_features(dataframe,e)
-        if is_formal_concept(dataframe,e,att):
-            res.append(FormalConcept(e,att))
-            
-    return res
-
-# test
-# FC=formal_concepts(df)
-# print([(e.entities,e.features) for e in FC])
+r_new=0
 
 
-# indique si le tableau t2 est inclu dans le tableau t1
-# prend en argument deux tableau différents et ayant des éléments tous différents
-# peut être amélioré (?) en ne considérant que le sous tableau de t1 restant après chaque itération --> voir complexité
 
-def is_included(t1,t2):
-    
+# fonction is_cannonical
+# prend en argument la matrice des données, des variables r et y,la liste lc des
+# concepts formels
+# renvoie un booléen indiquant si le concept formel lc[r_new] est canonique ou non
+# algorithme développé par S. Andrews (université de Sheffield)
+
+def is_cannonical(mat,r,y,lc):
+    global r_new
     res=True
-    n1=len(t1)
-    n2=len(t2)
+    A=lc[r_new].objects
+    B=lc[r].attributes
+    k=len(B)-1
+    while k >= 0 and res :
+        j=y
+        while j > B[k] and res :
+            h=0
+            still=True
+            while h < len(A) and still:
+                if mat[A[h]][j] == 0:
+                    still=False
+                h+=1
+            if h == len(A) and still:
+                res=False
+            j-=1
+        y=B[k]-1
+        k-=1
     
-    if n2 < n1:
-        i=0
-        while res and i<n2:
-            res=res and  (t2[i] in t1)
-            i+=1
-    
-    else:
-        res=False
-    
+    j=y
+    while j >= 0 and res:
+        h=0
+        still=True
+        while h < len(A):
+            if mat[A[h]][j] == 0:
+                still=False
+            h+=1
+        if h == len(A) and still:
+            res=False
+        j-=1
     return res
+
+
+
+
+# fonction in_close
+# prend en argument la matrice de données, des variables r et y initialisées à 0,
+# la liste des concepts initialisée à une case contenant l'infimum, le nombre n 
+# d'attributs
+# renvoie la liste de tous les concepts formels, sauf le supremum
+# algorithme développé par S. Andrews (université de Sheffield)
+
+def in_close(mat,r,y,lc,n):
+    global r_new
+    AB=lc[r]
+    r_new+=1
+    lc.append(FormalConcept([],[]))
+    for j in range(y,n):
+        lc[r_new].objects=[]
+        for i in AB.objects:
+            if mat[i][j] == 1:
+                lc[r_new].add_entity(i)
+        if lc[r_new].objects != [] :
+            if len(lc[r_new].objects) == len(AB.objects):
+                AB.add_feature(j)
+            elif is_cannonical(mat,r,j-1,lc):
+                lc[r_new].attributes=AB.attributes+[j]
+                in_close(mat,r_new,j+1,lc,n)
 
 # test
-# print(is_included([4,1,2,3],[3,5,2]))
 
-# insère récursivement un concept à sa place dans le treilli par récursion
-# si l'ensemble des objets du concept n'est inclu dans aucun enfants de la racine
-# on crée un arc entre la racine et le concept
-# sinon
-# on insère le concept dans les enfants correspondant
+'''l=[FormalConcept(range(5),[])]
+in_close(A,0,0,l,5)
+for e in l:
+    print(e)
+print(r_new)'''
 
-def insere_concept_e(concept,treilli):
-    
-    ch=treilli.children
-    arc=True
-    ent=concept.entities
-    
-    for c in ch:
-        if is_included(c.node.entities,ent):
-            arc=False
-            insere_concept_e(concept,c)
-    
-    if arc:
-        treilli.add_child(ConceptLattice(concept))
 
-# construit le treilli de concepts dont la racine est le concept contenant tous
-# les objets
 
-def concept_lattice_e(dataframe):
+#--------------------------------------------------------------------------#
+# Trouver les concepts formels et le treillis de concepts - Algorithme DFS #
+#--------------------------------------------------------------------------#
+
+# à partir  d'un ensemble d'attributs, renvoie tous les objets communs
+
+def common_objects(M, attributs):
     
-    FC=formal_concepts(dataframe)
-    FC2=FC[1:]
-    res=ConceptLattice(FC[0])
-    for concept in FC2:
-        insere_concept_e(concept,res)
-           
+    nattributes = len(attributs)
+    nobjects = len(M)
+    obj = range(nobjects)
+    res = [1]*nobjects
+    
+    for i in obj:
+        j = 0
+        while (j < nattributes) and (res[i]==1):
+            res[i] = (res[i] and M[i][attributs[j]])
+            j += 1
+    
+    return [e for e in obj if (res[e]==1)]
+
+
+# à partir d'un ensemble d'objets, renvoie la liste des attributs communs
+
+def common_attributes(M, objets):
+    
+    nobjects = len(objets)
+    nattributes = len(M[0])
+    att = range(nattributes)
+    res = [1]*nattributes
+    
+    for j in att:
+        i = 0
+        while (i < nobjects) and (res[j]==1):
+            res[j] = (res[j] and M[objets[i]][j])
+            i += 1
+    
+    return [e for e in att if (res[e]==1)]
+
+
+# prend en argument un attribut et une liste d'objets, et renvoie tous les objets
+# de cette liste en relation avec l'attribut
+
+def objr(M, att, obj):
+    
+    res = []
+    
+    for e in obj:
+        if M[e,att]:
+            res.append(e)
+    
     return res
 
-#test
-CL=concept_lattice_e(df)
 
-print("Racine")
-print(CL.node.entities)
-print(CL.node.features)
+# prend en argument un objet et une liste d'attributs, et renvoie tous les attributs
+# de cette liste en relation avec l'objet
 
-for e in CL.children:
-    print("Concept suivant")
-    print(e.node.entities)
-    print(e.node.features)
+def attrr(M, obj, att):
+    
+    res = []
+    
+    for e in att:
+        if M[obj,e]:
+            res.append(e)
+    
+    return res
+
+print(objr(A, 2, [1,2,4]))
+
+
+# prend en argument la liste de tous les attributs et un échantillon d'attributs
+# X, et renvoie L\X
+
+def without(L, X):
+    
+    res = []
+    Xbis = X+[-1]
+    n = len(L)
+    i = 0
+    j = 0
+    while j < n:
+        if L[j] == Xbis[i]:
+            i += 1
+            j += 1
+        else:
+            res.append(L[j])
+            j += 1
+    
+    return res

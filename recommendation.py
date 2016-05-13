24431d46b924290849c27025b076d27c31a04359
ClementGiron <gironclement@orange.fr>
2016-05-13 18:29:57 +0200

    ajout de l'algorithme de recommandation

--- recommendation.py
+++ recommendation.py
@@ -4,7 +4,14 @@
 # Libraries #
 #-----------#
 
+# concepts and lattices
 import concepts as cp
+
+# queue
+import queue as qu
+
+
+# compute the support of an attribute list
 
 def support(M,attr):
     """
@@ -12,7 +19,10 @@ def support(M,attr):
             - attr: attributes
     Output : |attr(I)''|
     """
-    return len(common_objects(M,attr))
+    return len(cp.common_objects(M,attr))
+
+
+# compute the union of the sorted lists attr1 and attr2
 
 def union(attr1,attr2):
     """
@@ -46,6 +56,9 @@ def union(attr1,attr2):
             i+=1
     return union
 
+
+# compute the support of the association rule attr1 -> attr2
+
 def supp(M,attr1,attr2):
     """
     Input : - M: context matrix
@@ -54,6 +67,9 @@ def supp(M,attr1,attr2):
     """
     return support(M,union(attr1,attr2))
 
+
+# indicate if c's attributes are frequent
+
 def frequence(c,th):
     """
     Input : - c: concept
@@ -61,6 +77,9 @@ def frequence(c,th):
     Output : freq(c) (bool)
     """
     return len(c.objects)>=th
+
+
+# compute the confidence of the association rule attr1 -> attr2
 
 def confiance(M,attr1,attr2):
     """
@@ -84,6 +103,8 @@ print(l[4])
 print(supp(M,l[3].attributes,l[4].attributes))
 '''
 
+# indicate if k is in obj, when obj is a sorted list
+
 def inobjects(k, obj):
     
     """
@@ -104,37 +125,165 @@ def inobjects(k, obj):
     
     return res
 
-print(inobjects(4, [0, 1, 2, 3, 5, 6, 7, 8, 9]))
 
+# compute the set of nodes where k appears and the number of objects is superior to the threshold
 
-# compute the set of last nodes where k appears in the concept lattice
-
-def find_last_occurences(L, k):
+def find_occurences(L, k, threshold):
     
     """
     Input : - L: concept lattice
             - k: object
-    Output : set of the last nodes where k appears
+            - threshold: integer
+    Output : set of the nodes where k appears and |objects| >= threshold
     """
     
     s = set()
     
-    for lat in L.children:
-        if inobjects(k, lat.node.objects):
-            s = s.union({lat})
-    
-    if len(s) == 0:
-        return {L}
+    if len(L.node.objects) < threshold:
+        return s
     else:
-        res = set()
-        for lat in s:
-            res = res.union(find_last_occurences(lat, k))
-        return res
+        s = s.union({L})
+        for lat in L.children:
+            if inobjects(k, lat.node.objects):
+                s = s.union(find_occurences(lat, k, threshold))
+        return s
 
-df = cp.create_tab(20,15)
+# give the elements that are in l2 but not in l1
+
+def extersection(l1, l2):
+    
+    """
+    Input : - l1, l2: sortedlists of integer
+    Output : l2\l1
+    """    
+    
+    n1 = len(l1)
+    n2 = len(l2)
+    
+    i = 0
+    j = 0
+    
+    res = []
+    
+    while i < n1 and j < n2:
+        if l1[i] < l2[j]:
+            i += 1
+        elif l1[i] > l2[j]:
+            res.append(l2[j])
+            j += 1
+        else:
+            i += 1
+            j += 1
+    
+    if i == n1:
+        while j < n2:
+            res.append(l2[j])
+            j += 1
+    
+    return res
+        
+
+# recommender system restricted to a node (give the recommended films near to a given node)
+
+def recommand_node(M, L, k, freq_threshold, conf_threshold):
+    
+    """
+    Input : - M: formal context
+            - L: a node of the concept lattice
+            - k: object
+            - freq_threshold: integer
+            - conf_threshold: float
+    Output : list containing the recommanded attributes and the confidence
+    """    
+    
+    Q = qu.Queue()
+    done = []
+    res = []
+    attk = cp.common_attributes(M, [k])
+    
+    for lat in L.children:
+        if not(inobjects(k, lat.node.objects)):
+            Q.put(lat)
+            done.append(lat)
+    
+    while not(Q.empty()):
+        
+        lat = Q.get()
+        c = confiance(M, L.node.attributes, lat.node.attributes)
+        
+        if frequence(lat.node, freq_threshold) and (c >= conf_threshold):
+            
+            exter = extersection(attk, lat.node.attributes)
+            for e in exter:
+                res.append((e, c))
+            
+            proches = []
+            for e in lat.children:
+                if not(inobjects(k, e.node.objects)):
+                    proches.append(e)
+            for e in lat.parents:
+                if not(inobjects(k, e.node.objects)):
+                    proches.append(e)            
+            for p in proches:
+                if not(p in done):
+                    done.append(p)
+                    Q.put(p)
+    
+    return res
+
+
+# given a list of couples (film, confidence), return a dictionnary where keys are 
+# the films and the value associated to a key is the max confidence
+
+def purify(l):
+    
+    """
+    Input : - l: list of couples (film, confidence)
+    Output : dictionnary where keys are films and values are max confidences
+    """
+    
+    res = {}
+    
+    for e in l:
+        res[e[0]] = max(e[1], res.get(e[0], 0))
+    
+    return res
+
+
+# recommender system
+
+def recommendation(M, L, k, freq_threshold, conf_threshold):
+    
+    """
+    Input : - M: formal context
+            - L: root of the concept lattice
+            - k: object
+            - freq_threshold: integer
+            - conf_threshold: float
+    Output : list containing the recommanded attributes and the confidence
+    """
+    
+    res = []
+    
+    nodes = list(find_occurences(L, k, freq_threshold))
+    
+    for n in nodes:
+        res += recommand_node(M, n, k, freq_threshold, conf_threshold)
+    
+    return purify(res)
+    
+
+# test
+
+df = cp.create_tab(30,25)
 print(df)
 A = df.as_matrix()
 t = cp.compute_lattice(A)
 print(t)
-s = find_last_occurences(t, 3)
-print(s)
+'''print(30*'-')
+print("Noeuds trouvés :")
+s = find_occurences(t, 3, 3)
+print(s)'''
+
+r = recommendation(A, t, 3, 3, 0.8)
+print(r)
